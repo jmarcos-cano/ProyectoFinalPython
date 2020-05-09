145363825f8b81c96676fce7b9d06ae9d52a65ad
Pjvl99 <pablovelasquez@ufm.edu>
2020-05-08 23:08:22 -0600

    Prueba 4

--- Proyectofinal1.py
+++ Proyectofinal1.py
@@ -6,4 +6,4 @@ with open('data.json') as file:
 #print("Bienvenido al contactbook.\n1.Agregar contactos\n2.Buscar contactos\n3.Listar contactos\n4.Borrar contactos\n5.Llamar contactos\n6.Enviar mensaje a contactos\n7.Enviar correo a contactos\n8.Exportar contactos\n9.Salir\n")
 #opcion = input("Elija una opcion: ")
 
-print("Lu mato a polo wey noooo")
+print("xd")
